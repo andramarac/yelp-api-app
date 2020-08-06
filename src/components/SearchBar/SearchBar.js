88c603230ed18936fd1942d61c90a4c9e556baa3
Andra Maraciuc <andramarac@gmail.com>
2020-08-06 08:30:58 +0200

    8:30

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,6 +8,24 @@ const sortByOptions = {
 };
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term:'',
+            location:'',
+            sortBy:'best_match'
+        };
+    }
+
+    getSortByClass(sortByOption) {
+        if (sortByOption === this.state.sortBy) {
+            return 'active';
+        }
+        return '';
+    }
+
+    handleSortByChange(sortByOptions)
+
     renderSortbyOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
             let sortByOptionValue = sortByOptions[sortByOption];
